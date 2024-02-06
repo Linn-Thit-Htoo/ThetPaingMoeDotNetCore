71ec507a7c5cbbe2939446ce4970ba00916a7694
Linn Thit <linnthit77387@gmail.com>
2024-02-06 20:17:21 +0630

    console app day 1

---
+++ ThetPaingMoeDotNetCore/ConsoleAppDay1/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // class.MethodName
+        Console.WriteLine("Line 1");
+        Console.Write("Line 2");
+        Console.WriteLine("Line 3");
+
+        Console.ReadKey();
+    }
+}
