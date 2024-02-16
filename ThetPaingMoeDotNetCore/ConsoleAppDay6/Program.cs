95a93e0a33e1562518cb2b8034752f3cdc823e03
Linn Thit <linnthit77387@gmail.com>
2024-02-16 20:05:49 +0630

    Day 6

---
+++ ThetPaingMoeDotNetCore/ConsoleAppDay6/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Greeting("Hello User!"); // param
+        //Sum(1, 2, 3);
+        //Console.WriteLine(Sum(1.5, 2.5));
+
+        Console.ReadKey();
+    }
+    public static void Greeting(string? str = "") // argument
+    {
+        Console.WriteLine(str);
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public static double Sum(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Sum(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+}
---
+++ ThetPaingMoeDotNetCore/Exercise2/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    static string adminEmail = "linnthit77387@gmail.com";
+    static string adminPassword = "123123";
+    public static void Main(string[] args)
+    {
+        string? email = "";
+        string? password = "";
+
+        Console.Write("Please enter your email: ");
+        email = Console.ReadLine();
+        ValidateEmail(email);
+
+        Console.Write("\nPlease enter your password: ");
+        password = Console.ReadLine();
+        ValidatePassword(password);
+
+        Console.WriteLine("\nSuccess!");
+    }
+
+    public static void ValidateEmail(string email)
+    {
+        if (email != adminEmail || string.IsNullOrEmpty(email))
+        {
+            do
+            {
+                Console.Write("\nYour email is invalid.\nPlease enter again: ");
+                email = Console.ReadLine();
+                if (email == adminEmail && !string.IsNullOrEmpty(email))
+                    break;
+            } while (true);
+        }
+    }
+    public static void ValidatePassword(string password)
+    {
+        if (password != adminPassword || string.IsNullOrEmpty(password))
+        {
+            do
+            {
+                Console.Write("\nYour password is invalid.\nPlease enter again: ");
+                password = Console.ReadLine();
+                if (password == adminPassword && !string.IsNullOrEmpty(password))
+                    break;
+            } while (true);
+        }
+    }
+}
