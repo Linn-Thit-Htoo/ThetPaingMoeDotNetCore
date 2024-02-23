313cbeb84e0ac9acd272dbd20693f3efe2c68f6f
Linn Thit <linnthit77387@gmail.com>
2024-02-23 20:51:21 +0630

    [Completed] Console App

---
+++ ThetPaingMoeDotNetCore/Day10-Abstraction/Program.cs
@@ -0,0 +1,23 @@
+﻿public abstract class Animal
+{
+    public abstract void AnimalSound(); // abstract method (does not have a body)
+    public void Sleep() => Console.WriteLine("zzz"); // regular method
+    //private abstract void PrivateMethod(); cannot be private
+}
+
+public class Cat : Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("Cat is making sound...");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal cat = new Cat();
+        cat.AnimalSound();
+    }
+}
